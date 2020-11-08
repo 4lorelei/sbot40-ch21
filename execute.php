ff2c6af4e129372c2811a5b3515f0bae4da3fddf
4lorelei <43756481+4lorelei@users.noreply.github.com>
2020-11-08 18:36:25 +0100

    Add files via upload

--- execute.php
+++ execute.php
@@ -1,8 +1,4 @@
 <?php
 //$star_code = 0x1F31F;
-$star_code = 0x2605;
-setlocale(LC_TIME, 'it_IT');
-date_default_timezone_set('Europe/Rome');
-$content = file_get_contents("php://input");
-$update = json_decode($content, true);
+
 print_r("PARTITO");
