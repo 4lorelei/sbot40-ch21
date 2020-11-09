84995e2ec49d0d38e674c5e97841ca699a218191
Bruno Sarto <br.sarto@gmail.com>
2020-11-09 01:15:23 +0100

    new

--- execute.php
+++ execute.php
@@ -4,7 +4,7 @@ $star_code = 0x2605;
 setlocale(LC_TIME, 'it_IT');
 date_default_timezone_set('Europe/Rome');
 $content = file_get_contents("php://input");
-$update = json_decode($content, true);
+$update = json_decode($content, true); 
 print_r("PARTITO"); 
 if(!$update)
 {
