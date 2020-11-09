a09b4f200bbabdc5b5c9191c8039911b77f8fad6
Bruno Sarto <br.sarto@gmail.com>
2020-11-09 01:24:36 +0100

    no message

--- execute.php
+++ execute.php
@@ -5,7 +5,6 @@ setlocale(LC_TIME, 'it_IT');
 date_default_timezone_set('Europe/Rome');
 $content = file_get_contents("php://input");
 $update = json_decode($content, true); 
-print_r("PARTITO"); 
 if(!$update)
 {
   exit;
