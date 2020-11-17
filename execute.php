037655a0e23fbe3cb614c695c2be996786d87a03
4lorelei <br.sarto@gmail.com>
2020-11-17 01:35:43 +0100

    no message

--- execute.php
+++ execute.php
@@ -71,7 +71,7 @@ $path_log='log.txt';
 $path_lock='lock.txt';
 
 //prefisso password quesiti di tipo link
-$prefisso_pwd='_____';
+$prefisso_pwd='x_____';
 
 // keyboard con emoticons
 $emo_help = "\xF0\x9F\x94\x8D";
